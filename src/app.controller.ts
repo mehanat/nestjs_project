8d7011087c21c2f61bbc58a8831b6f3fa073d840
mehanat <mehanat1996@gmail.com>
2021-03-30 13:49:59 +0300

    video

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import {Controller, Get, Header, HttpCode, Res} from '@nestjs/common';
 import { AppService } from './app.service';
+import * as fs from "fs";
 
 @Controller()
 export class AppController {
@@ -8,5 +9,23 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('video')
+  //@HttpCode(201)
+  @Header('Content-Type', 'video/mp4')
+  //@Header('Content-Length', '3364687')
+  async streamVideo(@Res() res) {
+    const path = 'assets/16061365924341.mp4'
+    const stat = fs.statSync(path)
+    const fileSize = stat.size
+    //console.log(res)
+
+    const head = {
+      'Content-Length': fileSize,
+      'Content-Type': 'video/mp4',
+    }
+    res.set('Content-Length', fileSize)
+    fs.createReadStream(path).pipe(res)
   }
 }
