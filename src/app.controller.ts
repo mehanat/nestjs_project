d3d9fe25d12f401b9bbfd21e500920101604d2f1
mehanat <mehanat1996@gmail.com>
2021-03-31 14:45:26 +0300

    video

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import {Controller, Get, Header, HttpCode, Res} from '@nestjs/common';
+import {Controller, Get, Header, HttpCode, Res, Headers} from '@nestjs/common';
 import { AppService } from './app.service';
 import * as fs from "fs";
 
@@ -12,20 +12,33 @@ export class AppController {
   }
 
   @Get('video')
-  //@HttpCode(201)
   @Header('Content-Type', 'video/mp4')
   //@Header('Content-Length', '3364687')
-  async streamVideo(@Res() res) {
+  async streamVideo(@Res() res,
+                    @Headers() headers) {
+
     const path = 'assets/16061365924341.mp4'
     const stat = fs.statSync(path)
     const fileSize = stat.size
-    //console.log(res)
+    const range = headers.range
+    console.log(fileSize)
 
-    const head = {
-      'Content-Length': fileSize,
-      'Content-Type': 'video/mp4',
+    if (range) {
+      console.log('Chunk!')
+      const parts = range.replace(/bytes=/, "").split("-")
+      const start = parseInt(parts[0], 10)
+      const end = parts[1] ? parseInt(parts[1]) : start + 50000
+      console.log(start + ' - ' + end)
+      res.set('Content-Length', end - start + 1)
+      res.set('Accept-Ranges', 'bytes')
+      res.set('Content-Range', `bytes ${start}-${end}/${fileSize}`)
+      res.status(206)
+      fs.createReadStream(path, {start, end}).pipe(res)
+    } else {
+      console.log('All file')
+      res.set('Content-Length', fileSize)
+      res.status(200)
+      fs.createReadStream(path).pipe(res)
     }
-    res.set('Content-Length', fileSize)
-    fs.createReadStream(path).pipe(res)
   }
 }
